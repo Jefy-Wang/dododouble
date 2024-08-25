e93a29061298bcf63329193ba4ccbd9e97647b38
Jefy-Wang <wwfhuo33@gmail.com>
2024-08-25 13:13:16 +0800

    feat: 完善 vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import GIT from './build/git'
+import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import GIT from './build/git'
 import vue from '@vitejs/plugin-vue'
 import zipPack from 'vite-plugin-zip-pack'
 import legacyPlugin from '@vitejs/plugin-legacy'
