11b88b28a483f74f54d779b37a4b46c4eb07204e
Jefy-Wang <wwfhuo33@gmail.com>
2024-08-25 12:30:26 +0800

    feat: 新增 shared 目录

---
+++ src/shared/index.js
@@ -0,0 +1,27 @@
+// 执行回调函数
+export function fnCommonRun(fn, ...args) {
+  if (typeof fn === 'function') fn(...args)
+}
+
+// 设置页面加载
+export function setPageLoading(loading = true, text = '3D 引擎计算中...') {
+  const Timeout = 200
+  const LoadingRootEl = document.getElementById('page-loading__container')
+  const LoadingTextEl = document.getElementById('page-loading__text')
+
+  LoadingTextEl.textContent = text
+
+  if (loading) {
+    LoadingRootEl.style.opacity = '1'
+    LoadingRootEl.style.zIndex = '99999'
+    console.log('页面级 Loading 进行中')
+  } else {
+    LoadingRootEl.style.opacity = '0'
+    LoadingRootEl.style.animation = `gradual-hide ${Timeout}ms linear 1`
+
+    setTimeout(() => {
+      LoadingRootEl.style.zIndex = '-9999'
+      console.log('页面级 Loading 已完成')
+    }, Timeout)
+  }
+}
