94dc56d31da868552a197b7bc336a5dd5a7fbc4d
Jefy-Wang <wwfhuo33@gmail.com>
2024-12-10 08:56:13 +0800

    feat: 禁止浏览器右键功能

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import { setPageLoading } from '@/shared'
 import App from './App.vue'
 import router from './router'
 
+document.oncontextmenu = () => false // 禁止浏览器右键功能
+
 setPageLoading() // 开启页面加载效果
 
 const app = createApp(App)
