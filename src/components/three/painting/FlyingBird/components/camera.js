a14c6885d0a0139ca96ef36edaf768e1e1c1b295
Jefy-Wang <wwfhuo33@gmail.com>
2024-12-16 11:21:10 +0800

    feat: 优化模型加载时机

--- src/components/three/painting/FlyingBird/components/camera.js
+++ src/components/three/painting/FlyingBird/components/camera.js
@@ -2,8 +2,8 @@
 import { PerspectiveCamera } from 'three'
 
 export function createCamera(option) {
-  const el = option.el
-  const aspect = el.clientWidth / el.clientHeight // 宽高比（又名：纵横比、长宽比）
+  const canvas = option.canvas
+  const aspect = canvas.clientWidth / canvas.clientHeight // 宽高比（又名：纵横比、长宽比）
   const camera = new PerspectiveCamera(
     35, // fov = Field Of View
     aspect, // aspect ratio (dummy value)
